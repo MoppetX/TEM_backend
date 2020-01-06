37c52bb1dcb028f1414a43e78066ca84f5be4bd5
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2020-01-06 23:13:27 +0100

    do not need these... only for test

--- server-start.js
+++ server-start.js
@@ -8,9 +8,9 @@ connectDB();
 const DEFAULT_PORT = 3000;
 const PORT = process.env.PORT || DEFAULT_PORT;
 
-console.log(process.env.ENV1);
-console.log(process.env.ENV2);
-console.log(process.env.ENV3);
+// console.log(process.env.ENV1);
+// console.log(process.env.ENV2);
+// console.log(process.env.ENV3);
 
 server.listen(PORT, () => {
   console.info(
